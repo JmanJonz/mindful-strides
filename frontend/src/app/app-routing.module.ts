c210ff0019e303125b6ed99e66efafebb2348586
JmanJonz <wjssaan@gmail.com>
2025-08-04 19:23:10 -0600

    got some basic stuff going

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HubComponent } from './hub/hub.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HubComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { HubComponent } from './hub/hub.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HubComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
